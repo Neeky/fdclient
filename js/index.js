31515e31241de56f80d4e71da2b10cf616350be3
Neeky <1721900707@qq.com>
2017-07-31 17:22:10 +0800

    17.07.31.01

--- js/index.js
+++ js/index.js
@@ -16,19 +16,25 @@ document.getElementById("btnReserveCollect").addEventListener('click',function()
     chrome.tabs.create({url:"http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2017/06/2017060716033391362.htm"});
 });
 
+
+
 document.getElementById('btnGetCompanyList').addEventListener('click',function (){
     // 
-    var xhr=new XMLHttpRequest();
-    xhr.open('GET','component/cninfo/get/task/');
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onreadystatechange = function(){
-　　　　if ( xhr.readyState == 4 && xhr.status == 200 ) {
-            console.log(xhr.response.company);
-            if(xhr.response.company[1] != 0){
-                chrome.tabs.create({url:xhr.response.company[1]});
-            }
-　　　　} 
-　　};
+    function funa(){
+        var xhr=new XMLHttpRequest();
+        xhr.open('GET','http://www.financedatas.com/component/cninfo/get/task/');
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.onreadystatechange = function(){
+　　　　        if ( xhr.readyState == 4 && xhr.status == 200 ) {
+                console.log(xhr.response.company);
+                if(xhr.response.company[1] != 0){
+                    chrome.tabs.create({url:xhr.response.company[1]});
+                }
+　　　　    } 
+　　      };
+    };
+    // funa();
+    setInterval(funa,3000);
 });
 
